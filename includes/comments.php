25180cb49f405e3be270aa9aee9df47dfb4c0d22
aquicor <aquinoacordero@gmail.com>
2023-07-17 19:30:00 +0200

    show comments

--- includes/comments.php
+++ includes/comments.php
@@ -1,14 +1,13 @@
 <?php
 if (isset($_POST['create_comment'])) {
-    $the_post_id = $_GET['p_id'];
-
+    $comment_post_id = $_GET['p_id'];
     $comment_author = $_POST['comment_author'];
     $comment_author = strtolower($comment_author);
     $comment_email = $_POST['comment_email'];
     $comment_content = $_POST['comment_content'];
 
     $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_date) 
-    VALUES ('{$the_post_id}', '{$comment_author}', '{$comment_email}', '{$comment_content}', now())";
+    VALUES ('{$comment_post_id}', '{$comment_author}', '{$comment_email}', '{$comment_content}', now())";
 
     $create_comment_query = mysqli_query($connection, $query);
 
@@ -46,35 +45,61 @@ if (isset($_POST['create_comment'])) {
         <img class="media-object" src="http://placehold.it/64x64" alt="">
     </a>
     <div class="media-body">
-        <h4 class="media-heading">Start Bootstrap
+        <h4 class="media-heading">User
             <small>August 25, 2014 at 9:30 PM</small>
         </h4>
         Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
     </div>
 </div>
 
-<!-- Comment -->
-<div class="media">
-    <a class="pull-left" href="#">
-        <img class="media-object" src="http://placehold.it/64x64" alt="">
-    </a>
-    <div class="media-body">
-        <h4 class="media-heading">Start Bootstrap
-            <small>August 25, 2014 at 9:30 PM</small>
-        </h4>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-        <!-- Nested Comment -->
-        <div class="media">
-            <a class="pull-left" href="#">
-                <img class="media-object" src="http://placehold.it/64x64" alt="">
-            </a>
-            <div class="media-body">
-                <h4 class="media-heading">Nested Start Bootstrap
-                    <small>August 25, 2014 at 9:30 PM</small>
-                </h4>
-                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </div>
+
+<?php
+$comment_post_id = $_GET['p_id'];
+
+$query = "SELECT * FROM comments WHERE comment_post_id='$comment_post_id'
+    ORDER BY comment_id DESC";
+$select_comment_query = mysqli_query($connection, $query);
+
+if (!$select_comment_query) {
+    die('Query Failed' . mysqli_error($connection));
+}
+
+while ($row = mysqli_fetch_array($select_comment_query)) {
+    $comment_date = $row['comment_date'];
+    $comment_content = $row['comment_content'];
+    $comment_author = $row['comment_author'];
+    $comment_approve = $row['comment_approve'];
+
+    $date_day = date("F j, Y", strtotime($comment_date));
+    $date_hour = date("H:i", strtotime($comment_date));
+
+    $comment_date_format = $date_day . " at " . $date_hour;
+
+    echo ($comment_approve) ? 
+    "<div class='media'>
+        <a class='pull-left' href='#'>
+            <img class='media-object' src='http://placehold.it/64x64' alt=''>
+        </a>
+        <div class='media-body'>
+            <h4 class='media-heading'>$comment_author
+                <small>$comment_date_format</small>
+            </h4>
+            $comment_content
         </div>
-        <!-- End Nested Comment -->
-    </div>
+    </div>" :
+    "<div class='media'>
+        <a class='pull-left' href='#'>
+            <img class='media-object' src='http://placehold.it/64x64' alt=''>
+        </a>
+        <div class='media-body'>
+            <h4 class='media-heading'>$comment_author
+                [ Eliminated ]
+            </h4>
+        </div>
+    </div>";
+}
+?>
+
+<!-- Comment -->
+
 </div>
